8d0506ca6999a7aa6c9bc5f572667a435a08de90
benoit <benoit.zenone@gmail.com>
2024-08-28 15:58:26 +0200

    maj add menu + header

---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/common/Header';
+
+export default function OverviewPage() {
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Overview" />
+    </div>
+  );
+}
