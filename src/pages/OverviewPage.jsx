78cd13d6a21aaa5a07415d29807ca15642cabe59
benoit <benoit.zenone@gmail.com>
2024-08-30 15:52:12 +0200

    add stats card

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Header from '../components/common/Header';
+import StatCard from '../components/common/StatCard';
+import { motion } from 'framer-motion';
 
 export default function OverviewPage() {
   return (
     <div className="flex-1 overflow-auto relative z-10">
       <Header title="Overview" />
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        ></motion.div>
+      </main>
     </div>
   );
 }
