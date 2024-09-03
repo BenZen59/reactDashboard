9df1cb2276671de5f7243d8a8e7a592cf5bd8a4a
benoit <benoit.zenone@gmail.com>
2024-09-03 15:18:10 +0200

    maj

---
+++ src/components/overview/SalesOverviewChart.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SalesOverviewChart() {
+  return (
+    <div>SalesOverviewChart</div>
+  )
+}
--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../components/common/Header';
 import StatCard from '../components/common/StatCard';
 import { motion } from 'framer-motion';
+import { Users,BarChart2, Zap, ShoppingBag } from  'lucide-react';
+import SalesOverviewChart from '../components/overview/SalesOverviewChart';
 
 export default function OverviewPage() {
   return (
@@ -13,7 +15,35 @@ export default function OverviewPage() {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
-        ></motion.div>
+        >
+          <StatCard
+            name="Total Sales"
+            icon={Zap}
+            value="$12,345"
+            color="#6366F1"
+          />
+          <StatCard
+            name="New Users"
+            icon={Users}
+            value="1,234"
+            color="#8B5CF6"
+          />
+          <StatCard
+            name="Total Products"
+            icon={ShoppingBag}
+            value="567"
+            color="#EC4899"
+          />
+          <StatCard
+            name="Conversion Rate"
+            icon={BarChart2}
+            value="12.5%"
+            color="#10B981"
+          />
+        </motion.div>
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
+          <SalesOverviewChart/>
+        </div>
       </main>
     </div>
   );
